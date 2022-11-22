f05b4ce8942b44707bd7dde6d035dcaa8a47a990
unknown <nkt14042001@gmail.com>
2022-11-22 13:19:50 +0700

    header + slider

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,12 @@
-import logo from './logo.svg';
-//import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-// import { BrowserRouter, Routes, Route,Router } from "react-router-dom"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import LogSign from "./components/User/LogSign.js";
+import './App.css';
+import Header from "./components/Layout/Header.js"
+import { BrowserRouter as Router } from "react-router-dom"
+import Slider from './components/Layout/Slider';
+//import LogSign from './component/layout/LogSign';
+const App = () => {
+  return (<><Header /><Slider /></>);
 
-//import Demo from './components/demo';
-//import User from "./components/User"
-//import a from "./a"
-function App() {
-  return (
-    // <BrowserRouter>
-
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/login" element={<LogSign />} />
-
-        </Routes>
-      </div>
-    </Router>
-
-  );
 }
-{/* </BrowserRouter> */ }
 
 export default App;
