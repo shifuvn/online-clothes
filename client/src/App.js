69259023c53b7b091fc0f032fe52ddb46c809932
unknown <nkt14042001@gmail.com>
2022-11-29 19:35:54 +0700

    components

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,17 @@ import Header from "./components/Header.js"
 import { BrowserRouter as Router } from "react-router-dom"
 import Slider from './components/Slider';
 import Categories from './components/Categories';
+import Products from './components/Products';
+import Newsletter from './components/Newslleter';
+import Footer from './components/Footer';
 //import LogSign from './component/layout/LogSign';
 const App = () => {
   return (<><Header />
     <Slider />
     <Categories />
+    <Products />
+    <Newsletter />
+    <Footer />
   </>);
 
 }
