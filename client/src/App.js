d6121ac9f8bd77d4008ae70d638fe2521ec1456d
unknown <nkt14042001@gmail.com>
2023-02-01 21:09:09 +0700

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 
-import './App.css';
-import Header from "./components/Header.js"
-import { BrowserRouter as Router } from "react-router-dom"
-import Slider from './components/Slider';
-import Categories from './components/Categories';
-import Products from './components/Products';
-import Newsletter from './components/Newslleter';
-import Footer from './components/Footer';
-//import LogSign from './component/layout/LogSign';
-const App = () => {
-  return (<><Header />
-    <Slider />
-    <Categories />
-    <Products />
-    <Newsletter />
-    <Footer />
-  </>);
+import React, { useState } from 'react';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import DetailProduct from './pages/DetailProduct/DetailProduct';
+import Home from './pages/Home/Home';
+import PageProduct from './pages/PageProduct/PageProduct';
+import { cartContext } from './Context/cartContext';
+import LogSign from './pages/Login/Login';
 
+function App() {
+  const [cart, setCart] = useState([]);
+  return (
+    <div>
+      <cartContext.Provider value={{ cart, setCart }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/detail' element={<DetailProduct />} />
+          <Route path='/login' element={<LogSign />} />
+          <Route path='/products' element={<PageProduct />} />
+          <Route path='/products/:id' element={<DetailProduct />} />
+        </Routes>
+      </cartContext.Provider>
+    </div>
+  );
 }
 
 export default App;
